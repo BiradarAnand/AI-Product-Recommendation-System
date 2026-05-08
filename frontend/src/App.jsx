959d5256bec0771690b6c440e86edc274077b006
BiradarAnand <biradaranand025@gmail.com>
2026-05-08 09:20:34 +0530

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home        from './pages/Home';
-import Register    from './pages/register';
-import Login       from './pages/login';
+import Register    from './pages/Register';
+import Login       from './pages/Login';
 import Cart        from './pages/Cart';
 import Wishlist    from './pages/Wishlist';
 import UnifiedChatbot from './pages/UnifiedChatbot'; // ← your merged chatbot file
