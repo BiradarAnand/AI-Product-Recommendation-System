239c42a821545fa4890b91fd67b1f4998b7e3eeb
BiradarAnand <biradaranand025@gmail.com>
2026-05-12 23:41:10 +0530

    update ap.py

--- backend/app.py
+++ backend/app.py
@@ -250,5 +250,11 @@ def register():
 
 
 # ─────────────────────────────────────────────────────────────────
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+    
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
